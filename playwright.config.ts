32f034d099cba6cabaae737ab489347035c75c6b
Duneth <duneth.chadeera@globalwavenet.com>
2025-08-22 18:02:11 +0530

    edit playwright.config.ts file

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,9 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
+    // Run tests in headless mode, which is required for CI/CD environments.
+    headless: true,
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
